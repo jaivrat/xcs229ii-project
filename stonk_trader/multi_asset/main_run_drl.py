eaedb53aa648c3fbebad94dba76e078b8fb48b76
jaivrat <jaivrat.singh@gmail.com>
2021-12-08 11:40:48 +0800

    main code under dev

--- stonk_trader/multi_asset/main_run_drl.py
+++ stonk_trader/multi_asset/main_run_drl.py
@@ -5,9 +5,36 @@ import os
 import time
 
 
+# Working data with technical indicators
+WORKING_DATA_WITH_TE_PATH = "data/wd_te.csv"
+
+
+def run_model():
+
+    # 1. Read the preprocessed working data from the file: We can move preprocessing to this code
+    #    later but as of now we should be happy with this
+    data  = pd.read_csv(WORKING_DATA_WITH_TE_PATH, parse_dates =['Date'])
+    data["Date"] = [d.date() for d in data.Date]
+    print("Working data with technical indicators details:")
+    print("data.shape:{data.shape}")
+    print(data.head())
+
+    pass
+
+
 
 if __name__ == "__main__":
     print("Running STONK trader model")
+
+    try:
+        run_model()
+    except Exception as e:
+        print(f"Got exception : {e}")
+        raise e
     
+    print("Finished STONK trader model.")
+
+
+
     
 
